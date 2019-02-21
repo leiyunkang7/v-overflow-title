fa52cfb73365025cd6fdb130bf5a8f853b7b5169
lyk <leiyunkang7@sina.com>
2019-02-21 17:23:48 +0800

    Create directives.js

---
+++ directives.js
@@ -0,0 +1,26 @@
+Vue.directive('overflow-title', function(el, { value, oldValue }){
+
+  if(value !== oldValue){
+
+    typeof(el.overflowTitleMouseover) === 'function' && el.removeEventListener('mouseover', el.overflowTitleMouseover)
+
+    el.overflowTitleMouseover = function () {
+
+      let span = document.createElement('span')
+    
+      span.innerText = this.innerText
+    
+      document.body.appendChild(span)
+    
+      if (this.offsetWidth < span.offsetWidth || this.parentNode.offsetWidth < this.parentNode.scrollWidth) {
+        this.setAttribute('title', value)
+      }else{
+        this.setAttribute('title', '')
+      }
+    
+      document.body.removeChild(span)
+    }
+
+    el.addEventListener('mouseover', el.overflowTitleMouseover)
+  }
+})
